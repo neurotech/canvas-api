cfa9fd4b04ee34fca2541eedb8350a445073e884
Tim Douglas <neurotech@gmail.com>
2015-07-16 11:01:55 +1000

    Updated SIS test to use environment variables for API key / domain

--- tests/sis.js
+++ tests/sis.js
@@ -1,11 +1,12 @@
 var test = require('tape');
-
 var canvas = require('../lib/canvas-api');
-var config = require('../config');
+
+var auth = { 'Authorization': 'Bearer ' + process.env.CANVAS_API_KEY };
+var domain = process.env.CANVAS_API_DOMAIN;
 
 test('Canvas API - SIS Import Status - Latest', function (t) {
   t.plan(2);
-  canvas.sisStatus(config.auth, config.domain)
+  canvas.sisStatus(auth, domain)
     .then(function (res) {
       t.equal(typeof res, 'object', 'Returned the latest SIS Import object.');
       t.equal(typeof res.id, 'number', 'The ID of the latest SIS Import object is a number.');
@@ -14,7 +15,7 @@ test('Canvas API - SIS Import Status - Latest', function (t) {
 
 test('Canvas API - SIS Import Status - ID of 1', function (t) {
   t.plan(1);
-  canvas.sisStatus(config.auth, config.domain, 1)
+  canvas.sisStatus(auth, domain, 1)
     .then(function (res) {
       t.equal(res.id, 1, 'Returned a SIS Import object with the ID of 1.');
     });
@@ -22,7 +23,7 @@ test('Canvas API - SIS Import Status - ID of 1', function (t) {
 
 test('Canvas API - SIS Import Status - All', function (t) {
   t.plan(1);
-  canvas.sisStatus(config.auth, config.domain, 'all')
+  canvas.sisStatus(auth, domain, 'all')
     .then(function (res) {
       t.equal(typeof res.sis_imports, 'object', 'Returned all SIS Import objects.');
     });
