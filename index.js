d226cda024757b0f4f9a602f7b4dff173950ea47
Tim Douglas <neurotech@gmail.com>
2017-01-19 20:12:14 +1100

    Added assignment module

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const helpers = require('./lib/helpers');
+const endpoint = require('./lib/endpoint');
 const sis = require('./lib/sis');
 const course = require('./lib/course');
-const endpoint = require('./lib/endpoint');
+const assignment = require('./lib/assignment');
 
-let canvas = { helpers, sis, course, endpoint };
+let canvas = { helpers, endpoint, sis, course, assignment };
 
 module.exports = canvas;
