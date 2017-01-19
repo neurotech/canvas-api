8b7c33feee1e4097687bc21771cd5c9846101c38
Tim Douglas <neurotech@gmail.com>
2017-01-19 20:12:39 +1100

    Added tests for generating assignment endpoints

--- tests/endpoint.js
+++ tests/endpoint.js
@@ -1,11 +1,13 @@
 const test = require('tape');
 const endpoint = require('../lib/endpoint');
 
+// Base URL
 test('Endpoints - Base URL', (t) => {
   t.plan(1);
   t.equal(typeof endpoint.base(), 'string');
 });
 
+// SIS
 test('Endpoints - SIS - Imports', (t) => {
   t.plan(1);
   t.equal(typeof endpoint.sis.imports(), 'string');
@@ -16,7 +18,19 @@ test('Endpoints - SIS - Upload', (t) => {
   t.equal(typeof endpoint.sis.upload(), 'string');
 });
 
+// Course
 test('Endpoints - Course - Migrate', (t) => {
   t.plan(1);
   t.equal(typeof endpoint.course.migrate(123), 'string');
+});
+
+// Assignment
+test('Endpoints - Assignment - Create', (t) => {
+  t.plan(1);
+  t.equal(typeof endpoint.assignment.create(123), 'string');
+});
+
+test('Endpoints - Assignment - Modify', (t) => {
+  t.plan(1);
+  t.equal(typeof endpoint.assignment.modify(123, 45678), 'string');
 });
